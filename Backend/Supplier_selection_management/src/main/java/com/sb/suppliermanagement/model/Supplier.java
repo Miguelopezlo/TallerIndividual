0f4f5c2406c9c1917d92b20957d3239892963d57
MIGUEL ANGEL LOPEZ LOPEZ <1005869418@bolivar.com>
2023-10-16 17:15:21 -0500

    Actualizando entidades en spring

--- Backend/Supplier_selection_management/src/main/java/com/sb/suppliermanagement/model/Supplier.java
+++ Backend/Supplier_selection_management/src/main/java/com/sb/suppliermanagement/model/Supplier.java
@@ -1,5 +1,57 @@
 package com.sb.suppliermanagement.model;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="supplier", schema="APP_M_PROV_SELECC")
 public class Supplier {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "supplier_secuence")
+	@SequenceGenerator(name = "supplier_secuence", sequenceName = "supplier_secuence", allocationSize = 1)
+	@Column(name="supplierid")
+	int supplierid;
+	
+	@Column(name="companyname")
+	String companyname;
+	
+	@Column(name="contact")
+	String contact;
+	
+	@Column(name="celphone")
+	String celphone;
+	
+	@Column(name="email")
+	String email;
+	
+	@Column(name="score")
+	String score;
+	
+	@Column(name="city")
+	String city;
+	
+	
+	@OneToMany(mappedBy = "supplierproductid")
+	private List<SupplierProductv1> supplier;
+	
+	@OneToMany(mappedBy = "supplierid")
+	private List<Contract> contract;
 
 }
--- Backend/Supplier_selection_management/src/main/java/com/sb/suppliermanagement/model/SupplierProductv1.java
+++ Backend/Supplier_selection_management/src/main/java/com/sb/suppliermanagement/model/SupplierProductv1.java
@@ -1,5 +1,30 @@
 package com.sb.suppliermanagement.model;
 
+
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+@Table(name="supplierproductv1", schema="APP_M_PROV_SELECC")
 public class SupplierProductv1 {
+    @Id
+	@JoinColumn(name = "supplierid")
+    private Long supplierid;
+    
+    @Id
+    @JoinColumn(name = "productid")
+    private Long productid;
 
 }

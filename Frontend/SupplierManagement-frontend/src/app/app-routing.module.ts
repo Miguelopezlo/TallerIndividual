3f9f453dd6961c1145c30f34c1d224c3635d388a
Jose Correa <correa.jose@correounivalle.edu.co>
2023-11-09 13:41:19 -0500

    Mini cambio

--- Frontend/SupplierManagement-frontend/src/app/app-routing.module.ts
+++ Frontend/SupplierManagement-frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { SupplierListComponent } from './supplier-list/supplier-list.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ContractListComponent } from './contract-list/contract-list.component';
+import { SupplierListIdComponent } from './supplier-list-id/supplier-list-id.component';
 
 const routes: Routes = [
   {path:'suppliers', component: SupplierListComponent},
   {path: 'products', component: ProductListComponent},
   {path: 'contracts', component: ContractListComponent},
+  {path: 'suppliersid', component: SupplierListIdComponent},
   { path: '**', redirectTo: '' }
 ];
 

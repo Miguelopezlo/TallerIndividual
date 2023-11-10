2ba285e815e4584ca31645061a79cac03d4a7c6b
MIGUEL ANGEL LOPEZ LOPEZ <1005869418@bolivar.com>
2023-11-09 20:07:47 -0500

    Añadiendo documentacion de servicios

--- Backend/Supplier_selection_management/src/main/java/com/sb/suppliermanagement/services/SupplierServiceImpl.java
+++ Backend/Supplier_selection_management/src/main/java/com/sb/suppliermanagement/services/SupplierServiceImpl.java
@@ -19,99 +19,92 @@ import com.sb.suppliermanagement.repository.SupplierRepository;
 @Service
 public class SupplierServiceImpl implements SupplierService {
 
-	@Autowired
-	private SupplierRepository repositorio;
-	
-	@Transactional(readOnly=true)
-	public List<Supplier> findAll() {
-		return repositorio.findAll();
-	}
+    @Autowired
+    private SupplierRepository repositorio;
 
+    // Método que busca y devuelve una lista de todos los proveedores.
+    @Transactional(readOnly=true)
+    public List<Supplier> findAll() {
+        return repositorio.findAll();
+    }
 
-	@Transactional(readOnly=true)
-	public Optional<Supplier> findById(Long id) {
-		return repositorio.findById(id);
-	}
-	
-	@Transactional(readOnly=true)
-	public List<Supplier> findByCity(String city) {
-		return (List<Supplier>) repositorio.findByCity(city);
-	}
-	
-	@Transactional(readOnly=true)
-	public List<Supplier> findByScore(int score) {
-		return (List<Supplier>) repositorio.findByScore(score);
-	}
+    // Método que busca y devuelve un proveedor específico por su ID.
+    @Transactional(readOnly=true)
+    public Optional<Supplier> findById(Long id) {
+        return repositorio.findById(id);
+    }
 
+    // Método que busca y devuelve una lista de proveedores por ciudad.
+    @Transactional(readOnly=true)
+    public List<Supplier> findByCity(String city) {
+        return (List<Supplier>) repositorio.findByCity(city);
+    }
 
-	@Transactional
-	public Supplier save(Supplier supplier) {
-		return repositorio.save(supplier);
-	}
+    // Método que busca y devuelve una lista de proveedores por puntaje.
+    @Transactional(readOnly=true)
+    public List<Supplier> findByScore(int score) {
+        return (List<Supplier>) repositorio.findByScore(score);
+    }
 
-	@Transactional
-	public void deleteById(Long id) {
-		repositorio.deleteById(id);
+    // Método que guarda un nuevo proveedor en la base de datos.
+    @Transactional
+    public Supplier save(Supplier supplier) {
+        return repositorio.save(supplier);
+    }
 
-	}
+    // Método que elimina un proveedor por su ID.
+    @Transactional
+    public void deleteById(Long id) {
+        repositorio.deleteById(id);
+    }
 
+    // Método que busca y devuelve una lista de proveedores por el ID del producto que suministran.
+    @Transactional(readOnly=true)
+    public List<Supplier> getSupplierByProductid(Long productid) {
+        System.out.println("Se están buscando los proveedores para este producto" + productid);
+        return repositorio.getSupplierByProductid(productid);
+    }
 
-	@Transactional(readOnly=true)
-	public List<Supplier> getSupplierByProductid(Long productid) {
-		System.out.println("Se están buscando los proveedores para este producto"+ productid);
-		return repositorio.getSupplierByProductid(productid);
-		
-	}
-	
-	
-	
-    // Documentation: https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/multipart/MultipartFile.html
-    // Reads the MultipartFile and reads CSV line by line to get data
-    // Data from each line is manually mapped to create Client instances and push them into a Clients list
-    // Supplier list is then saved in DB
+    // Método que crea proveedores a partir de un archivo CSV recibido como MultipartFile.
     public SupplierProcessingDTO createSuppliers(MultipartFile file) {
         SupplierProcessingDTO processInfo = new SupplierProcessingDTO();
         int newSupplierCount = 0;
-        // invalidDocuments will be given to front end for information purposes
         ArrayList<Long> invalidSupplierid = new ArrayList<Long>();
         ArrayList<Long> validSupplierid = new ArrayList<Long>();
         ArrayList<Supplier> toSaveSupplier = new ArrayList<Supplier>();
         try {
-            // Reads as text content from the file received as argument
+            // Lee el contenido del archivo como texto desde el MultipartFile.
             BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
-            // This variable is going to have each line records as one string
             String row;
             while ((row = reader.readLine()) != null && !row.isEmpty()) {
-                // Splits row string into different values
+                // Divide la cadena de fila en diferentes valores utilizando una coma como delimitador.
                 String[] supplierData = row.split(",");
-                System.out.println("Fila "+Arrays.toString(supplierData));
-                // Manually mapping into a Client instance
+                System.out.println("Fila " + Arrays.toString(supplierData));
+                // Mapea manualmente los datos en una instancia de Supplier.
                 Supplier newSupplier = new Supplier();
                 newSupplier.setSupplierid(Long.parseLong(supplierData[0]));
-                // Checks if supplierId exists in db or if it is repeated in this csv file (does that by seeing if supplierid exists in validId list)
-                if (repositorio.existsBySupplierid(newSupplier.getSupplierid()) || validSupplierid.contains(newSupplier.getSupplierid())==true) {
-                    // If client document is already in database or processed in this csv file
-                	invalidSupplierid.add(newSupplier.getSupplierid());
-                }
-                else {
-                	//Setting propierties to the temporal newSupplier object
-                	validSupplierid.add(newSupplier.getSupplierid());
+
+                // Comprueba si el supplierId ya existe en la base de datos o si se repite en este archivo CSV.
+                if (repositorio.existsBySupplierid(newSupplier.getSupplierid()) || validSupplierid.contains(newSupplier.getSupplierid()) == true) {
+                    invalidSupplierid.add(newSupplier.getSupplierid());
+                } else {
+                    validSupplierid.add(newSupplier.getSupplierid());
                     newSupplier.setCompanyname(supplierData[1].strip());
                     newSupplier.setContact(supplierData[2].strip());
                     newSupplier.setAddress(supplierData[3].strip());
                     newSupplier.setCelphone(supplierData[4].strip());
                     newSupplier.setEmail(supplierData[5].strip());
-                    newSupplier.setScore(Integer.parseInt(supplierData[6].strip()));
+                    newSupplier.setScore(Integer.parseInt(supplierData[6].strip())) ;
                     newSupplier.setCity(supplierData[7].strip());
                     toSaveSupplier.add(newSupplier);
-                 }
+                }
             }
-            // Number of saved clients
+            // Guarda la lista de proveedores en la base de datos y cuenta los nuevos proveedores creados.
             newSupplierCount = repositorio.saveAll(toSaveSupplier).size();
             processInfo.setCreatedSuppliers(newSupplierCount);
             processInfo.setInvalidSupplierid(invalidSupplierid);
-            System.out.println("INTERNAL PROCESS: " + newSupplierCount + " new clients were saved");
-            System.out.println("INTERNAL PROCESS: " + invalidSupplierid.size() + " invalid documents found");
+            System.out.println("INTERNAL PROCESS: " + newSupplierCount + " nuevos proveedores se han guardado");
+            System.out.println("INTERNAL PROCESS: " + invalidSupplierid.size() + " documentos inválidos encontrados");
             return processInfo;
         } catch (IOException ex) {
             ex.printStackTrace();
@@ -119,36 +112,28 @@ public class SupplierServiceImpl implements SupplierService {
         }
     }
 
+    // Método que busca y devuelve una lista de proveedores como objetos DTO.
+    @Transactional(readOnly=true)
+    public List<SupplierDTO> findAllasDTO() {
+        List<Supplier> supplier = repositorio.findAll();
+        List<SupplierDTO> supplierDTOs = new ArrayList<>();
+        // Recorre el arreglo supplier y settea los datos al objeto DTO
+        for (Supplier supplierite : supplier) {
+            SupplierDTO dto = new SupplierDTO();
+            dto.setCompanyname(supplierite.getCompanyname());
+            dto.setContact(supplierite.getContact());
+            dto.setAddress(supplierite.getAddress());
+            dto.setCelphone(supplierite.getCelphone());
+            dto.setEmail(supplierite.getEmail());
+            dto.setScore(supplierite.getScore());
+            dto.setCity(supplierite.getCity());
+            
+            //Agrega al arreglo supplierDTOs el objeto recien setteado
+            supplierDTOs.add(dto);
+        }
 
-	@Transactional
-	public List<SupplierDTO> findAllasDTO() {
-		List<Supplier> supplier = repositorio.findAll();
-		 List<SupplierDTO> supplierDTOs = new ArrayList<>();
-
-	        for (Supplier supplierite : supplier) {
-	            SupplierDTO dto = new SupplierDTO();
-	            dto.setCompanyname(supplierite.getCompanyname());
-	            dto.setContact(supplierite.getContact());
-	            dto.setAddress(supplierite.getAddress());
-	            dto.setCelphone(supplierite.getCelphone());
-	            dto.setEmail(supplierite.getEmail());
-	            dto.setScore(supplierite.getScore());
-	            dto.setCity(supplierite.getCity());
-
-//	            Obtener los nombres de las relaciones
-//	            if (contract.getProduct() != null) {
-//	                dto.setProductName(movement.getProductid().getProductid());
-//	            }
-//	            if (contract.getSupplier() != null) {
-//	                dto.setSupplierid(contractite.getSupplierid().getSupplierid());
-//	            }
-
-	            supplierDTOs.add(dto);
-	        }
-
-	        return supplierDTOs;
-	    }
-
+        return supplierDTOs;
+    }
 }
 	
 	
